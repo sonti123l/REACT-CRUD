aadff02c7ba9eccd570b80702e0112434de53f95
sonti123l <116090293+sonti123l@users.noreply.github.com>
2025-05-30 00:59:23 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,57 +1,78 @@
+import { useState } from 'react';
 import './App.css';
 import data from "./data.json";
 import PlayerDetails from './PlayerDetails';
-import { useState } from 'react';
+import EditDetails from './EditDetails';
+import AddPlayer from './AddPlayer';
+import DeletePlayer from './DeletePlayer';
 
 
 
 function App() {
 
-  // for adding a new player in the details section
+  // These are for players getting created
   const [players,setNewplayer] = useState(data.players);
-  const [newName,setNewName] = useState("");
-  const [newAge,setNewAge] = useState("");
-  const [newPosition,setNewPosition] = useState("");
   const [showForm,setShowForm] = useState(false);
+  // These are for players getting created
 
-  const handleUpdaterList = (e) => {
-    e.preventDefault();
-    const newPlayer = {
-      id : players.length + 1,
-      name : newName,
-      age : parseInt(newAge),
-      position : newPosition
-    }
-    setNewplayer([...players,newPlayer])
-    // after setting the player in the given data file then we need to clear the form data
-    setNewName("");
-    setNewAge("");
-    setNewPosition("");
-  }
+  // These are for players getting Editted
+  const [editId,setEditId] = useState(null);
+  const [showEditForm, setShowEditForm] = useState(false);
+  // These are for players getting Edited
 
+  // These are for player getting deleted
+  const [deleteId,setDeleteId] = useState(null);
+  const [showDeleteForm,setShowDeleteForm] = useState(false);
+
+  // These is to handle the addplayer component
   const handleShowForm = () => {
     console.log("Add Player clicked!");
     setShowForm(true);
   }
+ 
+   const handleAddPlayer = (newPlayer) => {
+    setNewplayer([...players, newPlayer]);
+    setShowForm(false); 
+  };
+  // These is to handle the addPlayer component
+
+  // here we will implement the edited details in the PlayerDetails
+  const handleEditId = (id) => {
+    console.log(id);
+    setEditId(id);
+    setShowEditForm(true);
+  }
+  // here we will implement the edited details in the PlayerDetails
+
+
+
+  // These is to handle edit details
+  const handleUpdateDetails = (updatedPlayer) => {
+        setNewplayer(players.map((player) => player.id === updatedPlayer.id?updatedPlayer:player));
+    };
+  // These is to handle edit details  
+
+  // These is to handle delete details
+  const handleDelete = (id) => {
+    setDeleteId(id);
+    setShowDeleteForm(true);
+  }
+
+  const handleDeleteDetails = (deleteId) => {
+    setNewplayer(players.filter((player) => player.id !== deleteId));
+    setShowDeleteForm(false);
+  }
+  // These is to handle delete details
 
   return(
     <>
-    <PlayerDetails Players={players} addPlayer={handleShowForm}/>
+    <PlayerDetails Players={players} addPlayer={handleShowForm} onEdit={handleEditId} onDelete={handleDelete}/>
     {showForm && (
-      <form onSubmit={handleUpdaterList}>
-      <label htmlFor='name'></label>
-      <input type='text' id='name' value={newName} onChange={(e) => setNewName(e.target.value)}></input>
-      <br></br>
-      <label htmlFor='age'></label>
-      <input type='text' id='age' value={newAge} onChange={(e) => setNewAge(e.target.value)}></input>
-      <br></br>
-      <label htmlFor='position'></label>
-      <input type='text' id='position' value={newPosition} onChange={(e) => setNewPosition(e.target.value)}></input>
-      <br></br>
-      <button type='submit'>Submit</button>
-      <button type= "button" onClick={() => setShowForm(false)}>cancel</button>
-    </form>
+      <AddPlayer Players={players} onAddPlayer={handleAddPlayer} onCancel={() => setShowForm(false)}/>
     )}  
+    {showEditForm && editId!== null && (<EditDetails Players={players} onUpdatePlayer={handleUpdateDetails} onCancel={() => setShowEditForm(false)}  playerID={editId}/>
+  )}
+  {showDeleteForm && deleteId !== null && (<DeletePlayer Players={players} onConfirmDelete={handleDeleteDetails} onCancel={() => setShowDeleteForm(false)} playerID= {deleteId}/>)}
     </>
   )
 }
