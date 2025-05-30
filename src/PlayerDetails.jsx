50c9cc8ae38a947551dc96fd7619382731795ba6
sonti123l <116090293+sonti123l@users.noreply.github.com>
2025-05-30 09:28:53 +0530

    Update PlayerDetails.jsx

--- src/PlayerDetails.jsx
+++ src/PlayerDetails.jsx
@@ -1,20 +1,26 @@
+import "./PlayerDetails.css";
+
 const PlayerDetails = ({Players,onEdit,onDelete,addPlayer}) => {
     return(
         <div className="DetailsName">
-            <h1>Player Details</h1>
+            <h1>CRUD APP FOR FOOTBALL PLAYERS TRACKING</h1>
                 {
                     Players.map((player) => (
-                        <div key={player.id}>
-                            <h1>Player ID: {player.id}</h1>
-                            <h1>Player Name: {player.name}</h1>
-                            <h1>Player Age: {player.age}</h1>
-                            <h1>Player Position: {player.position}</h1>
-                            <button onClick={() => onEdit?.(player.id)}>Edit Details</button>
-                            <button onClick={() => onDelete?.(player.id)}>Delete Player details</button>
+                        <div key={player.id} className="detailsContainer">
+                                <div className="DetailsDiv">
+                                    <h1>Player ID: {player.id}</h1>
+                                    <h1>Player Name: {player.name}</h1>
+                                    <h1>Player Age: {player.age}</h1>
+                                    <h1>Player Position: {player.position}</h1>
+                                </div>
+                                <div className="ButtonsInPlayer">
+                                    <button onClick={() => onEdit(player.id)}>Edit Player</button>
+                                    <button onClick={() => onDelete(player.id)}>Delete Player</button>
+                                </div>
                         </div>
                     ))
                 }
-            <button onClick={addPlayer}>Add player</button>
+            <button onClick={addPlayer} id="AddPlayer">Add player</button>
         </div>
     
     )
