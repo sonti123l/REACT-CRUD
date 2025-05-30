5c44bd58dfdff1d7e00f0e230d2f8f49d0699168
sonti123l <116090293+sonti123l@users.noreply.github.com>
2025-05-30 09:28:10 +0530

    Update DeletePlayer.jsx

--- src/DeletePlayer.jsx
+++ src/DeletePlayer.jsx
@@ -1,15 +1,23 @@
+import "./DeletePlayer.css";
+
+
 const DeletePlayer = ({Players,playerID, onConfirmDelete, onCancel}) => {
     const getPlayerWhichIsGoingToBeDeleted = Players.find((player) => player.id === playerID);
     return(
         <>
-            <h1>These are the details that you wanted to deleted</h1>
-            <div key={playerID}>
-                <h3>playerName: {getPlayerWhichIsGoingToBeDeleted.name}</h3>
-                <h3>playerAge: {getPlayerWhichIsGoingToBeDeleted.age}</h3>
-                <h3>playerPosition: {getPlayerWhichIsGoingToBeDeleted.position}</h3>
-                <button onClick={() => onConfirmDelete(playerID)}>Delete</button>
-                <button onClick={onCancel}>cancel</button>
-            </div>   
+            <div className="mainContainer">
+                <h1>Player Details</h1>
+                <div key={playerID} className="InsideDiv">
+                    <h3>playerName: {getPlayerWhichIsGoingToBeDeleted.name}</h3>
+                    <h3>playerAge: {getPlayerWhichIsGoingToBeDeleted.age}</h3>
+                    <h3>playerPosition: {getPlayerWhichIsGoingToBeDeleted.position}</h3>
+                    <div className="ButtonsDiv">
+                        <button onClick={() => onConfirmDelete(playerID)}>Delete</button>
+                        <button onClick={onCancel}>cancel</button>
+                    </div>
+                </div> 
+            </div>
+              
         </>
     )
 }
